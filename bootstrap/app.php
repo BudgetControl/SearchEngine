9f82eabc7377680436d39129438ca834cf416a2f
BudgetControl <140796512+BudgetControl@users.noreply.github.com>
2024-05-17 10:06:42 +0200

    Initial commit

---
+++ bootstrap/app.php
@@ -0,0 +1,67 @@
+<?php
+// Autoload Composer dependencies
+
+use \Illuminate\Support\Carbon as Date;
+use Illuminate\Support\Facades\Facade;
+use Monolog\Level;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+// Set up your application configuration
+// Initialize slim application
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+// Crea un'istanza del gestore del database (Capsule)
+$capsule = new \Illuminate\Database\Capsule\Manager();
+
+// Aggiungi la configurazione del database al Capsule
+$connections = require_once __DIR__.'/../config/database.php';
+$capsule->addConnection($connections['mysql']);
+
+// Esegui il boot del Capsule
+$capsule->bootEloquent();
+$capsule->setAsGlobal();
+
+//setup log level from env
+switch(env('APP_LOG_LEVEL','debug')) {
+    case 'debug':
+        $logLevel = Level::Debug;
+        break;
+    case 'info':
+        $logLevel = Level::Info;
+        break;
+    case 'notice':
+        $logLevel = Level::Notice;
+        break;
+    case 'warning':
+        $logLevel = Level::Warning;
+        break;
+    case 'error':
+        $logLevel = Level::Error;
+        break;
+    case 'critical':
+        $logLevel = Level::Critical;
+        break;
+    case 'alert':
+        $logLevel = Level::Alert;
+        break;
+    case 'emergency':
+        $logLevel = Level::Emergency;
+        break;
+    default:
+        $logLevel = Level::Debug;
+}
+
+$logPath = env('APP_LOG_PATH',__DIR__.'/../storage/logs/log-'.date("Ymd").'.log');
+$streamHandler = new \Monolog\Handler\StreamHandler($logPath, $logLevel);
+$logger = new \Monolog\Logger('MS-STATS');
+$formatter = new \Monolog\Formatter\SyslogFormatter();
+$streamHandler->setFormatter($formatter);
+$logger->pushHandler($streamHandler);
+
+// Set up the Facade application
+Facade::setFacadeApplication([
+    'log' => $logger,
+    'date' => new Date(),
+]);
