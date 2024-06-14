d17543feb7c8103f26107c6ecf71c0ad8734ab51
Marco De Felice <marco.defelice@qwentes.it>
2024-06-14 09:04:50 +0200

    added monitor

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,4 @@
 
 
  $app->post('/find/{wsid}', \Budgetcontrol\SearchEngine\Http\Controller\SearchController::class. ':find');
+$app->get('/monitor', \Budgetcontrol\SearchEngine\Http\Controller\Controller::class . ':monitor');
