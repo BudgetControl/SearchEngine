e474f4b8bae95e84c44f82bfc5d43c140b68f155
Marco De Felice <marco.defelice@budgetcontrol.cloud>
2025-07-07 11:09:56 +0200

    update

--- public/index.php
+++ public/index.php
@@ -1,10 +1,13 @@
 <?php
 
+
 require_once __DIR__ . "/../bootstrap/app.php";
 
-$ip = explode(',', $_SERVER['X-Forwarded-For'])[0] ?? $_SERVER['REMOTE_ADDR'];
-$ipAllowed = explode(',', getenv('IP_ALLOWLIST') ?? '');
-if (!in_array($ip, $ipAllowed) || $_SERVER['HTTP_X_API_SECRET'] !== getenv('SECRET_KEY')) {
+$ip = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? explode(',', $_SERVER['HTTP_X_FORWARDED_FOR'])[0] : $_SERVER['REMOTE_ADDR'] ?? '';
+$ipAllowed = explode(',', env('IP_ALLOWLIST'));
+\Illuminate\Support\Facades\Log::debug('IP Address: ' . $ip);
+
+if (!in_array($ip, $ipAllowed) || $_SERVER['HTTP_X_API_SECRET'] !== env('SECRET_KEY')) {
     http_response_code(403);
     echo 'Access Denied - You are not allowed to access this resource.';
     exit;
