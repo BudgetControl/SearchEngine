7272ec103ec9f4ed953bd6840b86ee9561aed017
Marco De Felice <marco.defelice@budgetcontrol.cloud>
2025-07-07 09:35:58 +0200

    block from ip

--- public/index.php
+++ public/index.php
@@ -2,6 +2,14 @@
 
 require_once __DIR__ . "/../bootstrap/app.php";
 
+$ip = explode(',', $_SERVER['X-Forwarded-For'])[0] ?? $_SERVER['REMOTE_ADDR'];
+$ipAllowed = explode(',', getenv('IP_ALLOWLIST') ?? '');
+if (!in_array($ip, $ipAllowed) || $_SERVER['HTTP_X_API_SECRET'] !== getenv('SECRET_KEY')) {
+    http_response_code(403);
+    echo 'Access Denied - You are not allowed to access this resource.';
+    exit;
+}
+
 $app = \Slim\Factory\AppFactory::create();
 
 /**
